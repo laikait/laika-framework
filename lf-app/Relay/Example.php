2987f0cd52f2371181abe837d8a69b9859492236
Laika IT <strblackhawk@gmail.com>
2026-04-14 23:21:21 +0600

    lf-app examples bug fixed

---
+++ lf-app/Relay/Example.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Laika Example Relay Provider
+ * Author: Showket Ahmed
+ * Email: riyadhtayf@gmail.com
+ * License: MIT
+ */
+
+declare(strict_types=1);
+
+namespace App\Relay;
+
+use Laika\Core\Relay\Relay;
+
+/**
+ * @method static void yourMethod()
+ */
+class Example extends Relay
+{
+    protected static function getRelayAccessor(): string
+    {
+        return 'example';
+    }
+}
